6f6ce374569e14564759641f45efaaf6a479bfaf
Jimmy Sherman <jsher3434@gmail.com>
2017-02-18 00:11:45 -0800

    Initial Picture game GUI

--- GEDAppGUI/app/src/main/java/com/gedappgui/gedappgui/ButtonAdapter.java
+++ GEDAppGUI/app/src/main/java/com/gedappgui/gedappgui/ButtonAdapter.java
@@ -18,11 +18,21 @@
 package com.gedappgui.gedappgui;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.os.Handler;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class ButtonAdapter extends BaseAdapter {
 
@@ -30,15 +40,29 @@ public class ButtonAdapter extends BaseAdapter {
     private Context mContext;
     // Hold the ids of the images from the drawable folder
     private String[] buttonNames;
+    private ImageView changer;
+    private String[] answers;
+    private TextView statement;
+    private int cur = 0;
+    private int pictureindex = 0;
+    private int[] pictures;
+    private TextView resulter;
 
     /*
      * Constructor
      * Gets the context and image ids so they can be used later
      */
-    public ButtonAdapter(Context c, String[] buttonNamesp) {
+    public ButtonAdapter(Context c, String[] buttonNamesp, ImageView change, String splitter,
+                         TextView state, int[] pics, TextView result) {
 
         mContext = c;
         buttonNames = buttonNamesp;
+        changer = change;
+        answers = splitter.split(",");
+        statement = state;
+        pictures = pics;
+        resulter = result;
+
     }
 
 
@@ -74,21 +98,102 @@ public class ButtonAdapter extends BaseAdapter {
      * Creates the components for the adapter
      * returns an imageview
      */
+
+    //credit: radhoo, StackOverflow
+    private static void ImageViewAnimatedChange(Context c, final ImageView v, final int new_image) {
+        final Animation anim_out = AnimationUtils.loadAnimation(c, android.R.anim.fade_out);
+        final Animation anim_in  = AnimationUtils.loadAnimation(c, android.R.anim.fade_in);
+        anim_out.setAnimationListener(new Animation.AnimationListener()
+        {
+            @Override public void onAnimationStart(Animation animation) {}
+            @Override public void onAnimationRepeat(Animation animation) {}
+            @Override public void onAnimationEnd(Animation animation)
+            {
+                v.setImageResource(new_image);
+                anim_in.setAnimationListener(new Animation.AnimationListener() {
+                    @Override public void onAnimationStart(Animation animation) {}
+                    @Override public void onAnimationRepeat(Animation animation) {}
+                    @Override public void onAnimationEnd(Animation animation) {}
+                });
+                v.startAnimation(anim_in);
+            }
+        });
+        v.startAnimation(anim_out);
+    }
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
+
 
         Button button;
         if (convertView == null) {
              //if it's not recycled, initialize some attributes
             button = new Button(mContext);
             button.setLayoutParams(
-                    new GridView.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+                    new GridView.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
             button.setPadding(8, 8, 8, 8);
         } else {
             button = (Button) convertView;
         }
 
         button.setText(buttonNames[position]);
+        if (button.getText().equals("TRUE")){
+            button.getBackground().setColorFilter(0xFF23c438, PorterDuff.Mode.MULTIPLY);
+            button.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v){
+                    if (answers[cur + 1].equals("t")){
+                        cur += 2;
+                        statement.setText(answers[cur]);
+                        resulter.setText("Correct!");
+                        Runnable r = new Runnable(){
+                            @Override
+                            public void run(){
+                                ImageViewAnimatedChange(mContext,changer,pictures[pictureindex]);
+                            }
+                        };
+                        Handler h = new Handler();
+                        h.postDelayed(r,750);
+                        pictureindex++;
+
+                    }
+                    else{
+                        resulter.setText("Incorrect! Try again");
+                    }
+                }
+            });
+        }
+        else if (button.getText().equals("FALSE")){
+            button.getBackground().setColorFilter(0xFFce4257, PorterDuff.Mode.MULTIPLY);
+            button.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v){
+                    if (answers[cur + 1].equals("f")){
+                        cur += 2;
+                        statement.setText(answers[cur]);
+                        resulter.setText("Correct!");
+                        Runnable r = new Runnable(){
+                            @Override
+                            public void run(){
+                                //changer.setImageResource(pictures[pictureindex]);
+                                ImageViewAnimatedChange(mContext,changer,pictures[pictureindex]);
+                            }
+                        };
+                        Handler h = new Handler();
+                        h.postDelayed(r,750);
+                        pictureindex++;
+
+                    }
+                    else{
+                        resulter.setText("Incorrect! Try again");
+
+                    }
+                }
+            });
+        }
+        button.setTextColor(Color.WHITE);
+        statement.setText(answers[cur]);
         return button;
     }
+
+
 }
--- GEDAppGUI/app/src/main/java/com/gedappgui/gedappgui/PictureGameView.java
+++ GEDAppGUI/app/src/main/java/com/gedappgui/gedappgui/PictureGameView.java
@@ -2,11 +2,14 @@ package com.gedappgui.gedappgui;
 
 import android.content.Context;
 import android.graphics.Color;
+import android.support.annotation.DrawableRes;
 import android.support.v4.content.ContextCompat;
 import android.view.Gravity;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.GridLayout;
 import android.widget.GridView;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -17,42 +20,90 @@ import android.widget.TextView;
 public class PictureGameView extends LinearLayout {
 
     private TextView statement;
+    private TextView result;
     private GridView buttons;
     private Button truthbutton;
-    private Context contextp;
+    private Context context;
+    private ImageView changer;
     private String[] texts = {
             "TRUE",
             "FALSE"
+    };
+    private String splits = "|-4| - 2 = 2,t,|-2| = 3,f,|4| - |-4| = 4,f,-5 + |20| = 15,t";
+    private int[] abs_pics = {
+            R.drawable.sprite_dragon,
+            R.drawable.settings,
+            R.drawable.home_screen,
+            R.drawable.home_screen_continue,
+            R.drawable.cylinder
+
     };
 
     public PictureGameView(Context contextp){
         super(contextp);
 
+        context = contextp;
+        statement = new TextView(context);
+        result = new TextView(context);
+        int[] pass_pics;
 
-        statement = new TextView(contextp);
+        if (true){
+            pass_pics = abs_pics;
+        }
+
+        this.setOrientation(LinearLayout.VERTICAL);
+        this.setLayoutParams(new LayoutParams( LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
+        this.setOrientation(LinearLayout.VERTICAL);
+
 
         LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
                 LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT);
-        layoutParams.setMargins(10, 10, 10, 10);
+        layoutParams.setMargins(10, 80, 10, 80);
         statement.setLayoutParams(layoutParams);
 
-        statement.setText("statement");
+        LinearLayout.LayoutParams linearLayout = new LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        linearLayout.setMargins(10, 10, 10, 10);
+
+        statement.setText("testing");
         statement.setTextSize(20);
         statement.setTextColor(Color.WHITE);
         statement.setGravity(Gravity.CENTER);
 
         this.addView(statement);
 
+        changer = new ImageView(context);
+        changer.setImageResource(R.drawable.cone);
+        changer.setLayoutParams(linearLayout);
+
         buttons = new GridView(contextp);
         buttons.setNumColumns(2);
+        buttons.setColumnWidth(100);
         buttons.setHorizontalSpacing(10);
         buttons.setVerticalSpacing(10);
-        buttons.setBackgroundColor(ContextCompat.getColor(contextp, R.color.bucketGameBG));
+
+        layoutParams.setMargins(10, 80, 10, 80);
+        result.setLayoutParams(layoutParams);
+        result.setText("");
+        result.setTextSize(20);
+        result.setTextColor(Color.WHITE);
+        result.setGravity(Gravity.CENTER);
 
 
 
-        truthbutton = new Button(contextp);
-        truthbutton.setText("button");
+        buttons.setLayoutParams(linearLayout);
+        buttons.setAdapter(new ButtonAdapter(context, texts, changer, splits, statement, pass_pics,result ));
+
+        this.addView(buttons);
+
+
+
+
+        this.addView(result);
+
+
+
+        this.addView(changer);
 
 
     }
